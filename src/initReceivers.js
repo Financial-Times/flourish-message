52a48af9709b41c59960a9f44c6490137115594d
Ændra Rininsland <hello@aendra.com>
2021-09-27 13:38:24 +0100

    Adding new workflow for pushed tags

--- src/initReceivers.js
+++ src/initReceivers.js
@@ -15,14 +15,21 @@ export const initReceivers = (userListeners, filterOrigin) => {
   const ALLOWED_HOSTS = ["flourish-api.com", "flo.uri.sh"];
 
   return (event) => {
-    const { data } = event;
-    const type = camelCase(data.type.toLowerCase());
-    const { hostname } = new URL(event.origin);
-    if (
-      listeners[type] &&
-      listeners[type] instanceof Function &&
-      (filterOrigin ? ALLOWED_HOSTS.includes(hostname) : true)
-    )
-      listeners[type](data.payload);
+    try {
+      const { data } = event;
+
+      if (!data || !data.type || !data.payload) return;
+
+      const type = camelCase(data.type.toLowerCase());
+      const { hostname } = new URL(event.origin);
+      if (
+        listeners[type] &&
+        listeners[type] instanceof Function &&
+        (filterOrigin ? ALLOWED_HOSTS.includes(hostname) : true)
+      )
+        listeners[type](data.payload);
+    } catch (e) {
+      console.error(`Event error`, e, event);
+    }
   };
 };
